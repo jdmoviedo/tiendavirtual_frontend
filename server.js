ffc49cea2bfce79cf935647c59f43b3192ebdcfb
Juan Mendoza <jdmo0802@gmail.com>
2021-12-03 00:22:39 -0500

    cambios

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist//frontend/'}),
+    res.sendFile('index.html', {root: 'dist/frontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
